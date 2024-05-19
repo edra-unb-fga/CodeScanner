fe1caf6ea03e7dafe1c0ab60e13ee6fa80af633e
Chronous <lucasmateustds@gmail.com>
2024-05-19 16:39:05 -0300

    :recycle: refactor: Atualizando o codigo para que ele reconheça a camera e não mais apenas fotos estaticas

--- main.py
+++ main.py
@@ -1,45 +1,38 @@
-# Importing library 
-import cv2 
-from pyzbar.pyzbar import decode 
-   
-# Make one method to decode the barcode  
-def BarcodeReader(image): 
-      
-    # read the image in numpy array using cv2 
-    img = cv2.imread(image) 
-       
-    # Decode the barcode image 
-    detectedBarcodes = decode(img) 
-       
-    # If not detected then print the message 
-    if not detectedBarcodes: 
-        print("Barcode Not Detected or your barcode is blank/corrupted!") 
-    else: 
-        
-          # Traverse through all the detected barcodes in image 
-        for barcode in detectedBarcodes:   
-            
-            # Locate the barcode position in image 
-            (x, y, w, h) = barcode.rect 
-              
-            # Put the rectangle in image using  
-            # cv2 to highlight the barcode 
-            cv2.rectangle(img, (x-10, y-10), 
-                          (x + w+10, y + h+10),  
-                          (255, 0, 0), 2) 
-              
-            if barcode.data!="": 
-                
-            # Print the barcode data 
-                print(barcode.data) 
-                print(barcode.type) 
-                  
-    #Display the image 
-    cv2.imshow("Image", img) 
-    cv2.waitKey(0) 
-    cv2.destroyAllWindows() 
-  
-if __name__ == "__main__": 
-  # Take the image from user 
-    image="Img.jpg"
-    BarcodeReader(image) 
+import cv2
+from pyzbar import pyzbar
+
+def decode_barcode(frame):
+    barcodes = pyzbar.decode(frame)
+    for barcode in barcodes:
+        (x, y, w, h) = barcode.rect
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        barcode_data = barcode.data.decode('utf-8')
+        barcode_type = barcode.type
+
+        text = f'{barcode_data} ({barcode_type})'
+        print(barcode_data)
+        cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    return frame, barcodes
+
+def main():
+    cap = cv2.VideoCapture(3)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame, barcodes = decode_barcode(frame)
+
+        cv2.imshow('Barcode Scanner', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
