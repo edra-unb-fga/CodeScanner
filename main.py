b72bae1d8dd73b319af0481f823bb7783cdbffc5
Chronous <lucasmateustds@gmail.com>
2024-05-24 10:45:47 -0300

    :sparkles: feat: Arquivo de configuração criado

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import cv2
+import config
 from pyzbar import pyzbar
 
 def decode_barcode(frame):
     barcodes = pyzbar.decode(frame)
-    for barcode in barcodes:
-        (x, y, w, h) = barcode.rect
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    if(config.ESCANEAR_CODIGO_DE_BARRA == True):
+        for barcode in barcodes:
+            (x, y, w, h) = barcode.rect
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        barcode_data = barcode.data.decode('utf-8')
-        barcode_type = barcode.type
+            barcode_data = barcode.data.decode('utf-8')
+            barcode_type = barcode.type
+            #barcode_data, barcode_type retornar esses valores para o ROS2 
 
-        text = f'{barcode_data} ({barcode_type})'
-        print(barcode_data)
-        cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
+            print(barcode_data)
+            text = f'{barcode_data} ({barcode_type})'
+            cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
     return frame, barcodes
 
 def main():
-    cap = cv2.VideoCapture(3)
+    cap = cv2.VideoCapture(config.CAMERA_PRINCIPAL)
 
     while True:
         ret, frame = cap.read()
